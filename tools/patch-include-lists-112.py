058f099d1486509cada7ececb632534009ea8bf4
Ben <hostellerie.org@gmail.com>
2026-08-20 15:08:57 +0200

    Documents 1.1.2: apply second atomic list cleanup

--- tools/patch-include-lists-112.py
+++ tools/patch-include-lists-112.py
@@ -13,16 +13,56 @@ def replace_once(old, new, label):
     text = text.replace(old, new, 1)
 
 
-replace_once('| Documents Plugin 1.0', '| Documents Plugin 1.1.2', 'header version')
-replace_once('Copyright (C) 2012 by the following authors:', 'Copyright (C) 2012-2026 by the following authors:', 'copyright')
 replace_once(
-    '    global $_CONF, $_DOCUMENTS_CONF, $LANG_DOCUMENTS_1,$_TABLES;',
-    "    global $_CONF, $_DOCUMENTS_CONF, $LANG_DOCUMENTS_1, $_TABLES, $_USER;\n\n    $retval = '';\n    $doc_titles = '';\n    $edit = '';",
-    'category callback globals and initialization'
+    "    $retval = '';\n\n\tif ($cat == '') return $retval;",
+    "    $retval = '';\n    $morefields = '';\n    $leftjoin = '';\n\n\tif ($cat == '') return $retval;",
+    'list docs accumulator initialization'
+)
+
+replace_once(
+    '\tdefine("CAT_URL", $cat);',
+    "\tif (!defined('CAT_URL')) {\n\t\tdefine('CAT_URL', $cat);\n\t}",
+    'category url constant guard'
+)
+
+replace_once(
+    "\t$category = DB_fetchArray(DB_query($sql));\n\t\n\t//is cat submitable",
+    "\t$category = DB_fetchArray(DB_query($sql));\n\tif (!is_array($category) || empty($category['cid'])) {\n\t\treturn $retval;\n\t}\n\n\t//is cat submitable",
+    'category result guard'
+)
+
+replace_once(
+    "\tif ($category['map'] != '' && $category['map'] > 0) $retval .= PLG_replaceTags(\"[maps:{$category['map']}]\");",
+    "\tif (DOCUMENTS_hasMaps() && $category['map'] != '' && $category['map'] > 0) {\n\t\t$retval .= PLG_replaceTags(\"[maps:{$category['map']}]\");\n\t}",
+    'category maps output guard'
+)
+
+replace_once(
+    "    global $_DOCUMENTS_CONF, $_CONF, $LANG_DOCUMENTS_1;\n\n\tswitch($fieldname) {",
+    "    global $_DOCUMENTS_CONF, $_CONF, $LANG_DOCUMENTS_1;\n\n    $retval = '';\n    $edit = '';\n    $inactive = '';\n\n\tswitch($fieldname) {",
+    'document callback initialization'
+)
+
+replace_once(
+    ". $_DOCUMENTS_CONF['site_url'] . '/image.php?src=' . $_DOCUMENTS_CONF['images_url'] \n\t\t\t\t\t. $B['v_value'] . '&amp;w=75&amp;h=75&amp;q=90&amp;zc=1\"",
+    ". $_DOCUMENTS_CONF['site_url'] . '/image.php?src=' . rawurlencode(basename($B['v_value'])) \n\t\t\t\t\t. '&amp;w=75&amp;h=75\"",
+    'category image preview endpoint'
+)
+
+replace_once(
+    "$image = '<img class=\"document_img\" src=\"' . $_DOCUMENTS_CONF['site_url'] . '/image.php?src=' . $_DOCUMENTS_CONF['images_url'] .\n\t\t\t\t\t\t$fieldvalue . '&amp;w=200&amp;h=200\" align=\"top\" alt=\"\" />';",
+    "$image = '<img class=\"document_img\" src=\"' . $_DOCUMENTS_CONF['site_url'] . '/image.php?src=' . rawurlencode(basename($fieldvalue)) .\n\t\t\t\t\t\t'&amp;w=200&amp;h=200\" align=\"top\" alt=\"\" />';",
+    'document image preview endpoint'
+)
+
+replace_once(
+    "\t\t\t\t} else if ($fieldvalue == $A['marker'] && $fieldvalue != '') {\n\t\t\t\t\t$retval = PLG_replaceTags('<div style=\"width:450px;\">[marker:' . $fieldvalue . ' width:400px]</div>');",
+    "\t\t\t\t} else if ($fieldvalue == $A['marker'] && $fieldvalue != '') {\n\t\t\t\t\tif (DOCUMENTS_hasMaps()) {\n\t\t\t\t\t\t$retval = PLG_replaceTags('<div style=\"width:450px;\">[marker:' . $fieldvalue . ' width:400px]</div>');\n\t\t\t\t\t} else {\n\t\t\t\t\t\t$retval = '';\n\t\t\t\t\t}",
+    'document marker output guard'
 )
 
 if text == original:
     raise RuntimeError('No changes were produced')
 
 path.write_text(text, encoding='utf-8')
-print('include_lists.php atomic category cleanup applied successfully')
+print('include_lists.php second atomic cleanup applied successfully')
