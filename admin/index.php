ceb70e3873bc27f80dbe4079f8ddebb2030736c9
Ben <hostellerie.org@gmail.com>
2026-08-20 10:31:25 +0200

    Stabilize Documents admin entry point

--- admin/index.php
+++ admin/index.php
@@ -2,65 +2,54 @@
 
 /* Reminder: always indent with 4 spaces (no tabs). */
 // +---------------------------------------------------------------------------+
-// | Documents Plugin 1.0                                                      |
+// | Documents Plugin 1.1.1                                                    |
 // +---------------------------------------------------------------------------+
 // | index.php                                                                 |
 // |                                                                           |
-// | Plugin administration page                                                |
+// | Plugin administration entry point.                                        |
 // +---------------------------------------------------------------------------+
-// | Copyright (C) 2012 by the following authors:                              |
+// | Copyright (C) 2012-2026 by the following authors:                         |
 // |                                                                           |
 // | Authors: Ben - ben AT geeklog DOT fr                                      |
-// +---------------------------------------------------------------------------+
-// | Created with the Geeklog Plugin Toolkit.                                  |
+// |          Documents plugin contributors                                    |
 // +---------------------------------------------------------------------------+
 // |                                                                           |
 // | This program is free software; you can redistribute it and/or             |
 // | modify it under the terms of the GNU General Public License               |
 // | as published by the Free Software Foundation; either version 2            |
 // | of the License, or (at your option) any later version.                    |
-// |                                                                           |
-// | This program is distributed in the hope that it will be useful,           |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of            |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
-// | GNU General Public License for more details.                              |
-// |                                                                           |
-// | You should have received a copy of the GNU General Public License         |
-// | along with this program; if not, write to the Free Software Foundation,   |
-// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           |
-// |                                                                           |
 // +---------------------------------------------------------------------------+
 
 /**
-* @package Documents
-*/
+ * @package Documents
+ */
 
 require_once '../../../lib-common.php';
 require_once '../../auth.inc.php';
 
 $display = '';
 
-// Ensure user even has the rights to access this page
-if (! SEC_hasRights('documents.admin')) {
-    $display .= COM_siteHeader('menu', $MESSAGE[30])
-             . COM_showMessageText($MESSAGE[29], $MESSAGE[30])
-             . COM_siteFooter();
+if (!SEC_hasRights('documents.admin')) {
+    $username = isset($_USER['username']) ? $_USER['username'] : 'unknown';
 
-    // Log attempt to access.log
-    COM_accessLog("User {$_USER['username']} tried to illegally access the Documents plugin administration screen.");
+    $display .= COM_siteHeader('menu', $MESSAGE[30]);
+    $display .= COM_showMessageText($MESSAGE[29], $MESSAGE[30]);
+    $display .= COM_siteFooter();
 
-    echo $display;
+    COM_accessLog(
+        'User ' . $username .
+        ' tried to illegally access the Documents plugin administration screen.'
+    );
+
+    COM_output($display);
     exit;
 }
 
-
-// MAIN
 $display .= COM_siteHeader('menu', $LANG_DOCUMENTS_1['plugin_name']);
 $display .= COM_startBlock($LANG_DOCUMENTS_1['plugin_name']);
-$display .= '<p>Admin!</p>';
+$display .= '<p><a href="' . $_DOCUMENTS_CONF['site_url'] . '">'
+    . $LANG_DOCUMENTS_1['plugin_name'] . '</a></p>';
 $display .= COM_endBlock();
 $display .= COM_siteFooter();
 
 COM_output($display);
-
-?>
