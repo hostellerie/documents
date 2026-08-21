1d5016a6e8cfa6df045ff43c6062139a4cc64576
Ben <hostellerie.org@gmail.com>
2026-08-21 16:13:13 +0200

    Documents 1.1.8: make image configuration upgrade explicit

--- install_updates.php
+++ install_updates.php
@@ -2,7 +2,7 @@
 
 /* Reminder: always indent with 4 spaces (no tabs). */
 // +---------------------------------------------------------------------------+
-// | Documents Plugin 1.1.2                                                    |
+// | Documents Plugin 1.1.8                                                    |
 // +---------------------------------------------------------------------------+
 // | install_updates.php                                                       |
 // |                                                                           |
@@ -12,11 +12,15 @@
 /**
  * Add image-limit settings to an existing Documents configuration group.
  *
+ * The Geeklog configuration API does not overwrite an existing value when
+ * add() is called for a parameter that is already present, so this helper is
+ * safe to run more than once.
+ *
  * @return bool
  */
-function DOCUMENTS_updateConfig_1_1_2()
+function DOCUMENTS_updateConfig_1_1_8()
 {
-    global $_CONF, $_DOCUMENTS_DEFAULT;
+    global $_CONF;
 
     require_once $_CONF['path_system'] . 'classes/config.class.php';
     require_once $_CONF['path'] . 'plugins/documents/install_defaults.php';
@@ -31,4 +35,15 @@ function DOCUMENTS_updateConfig_1_1_2()
     DOCUMENTS_addImageConfigItems($c, $me);
 
     return true;
+}
+
+/**
+ * Compatibility alias for development builds that already used the temporary
+ * 1.1.2 helper name before the configuration work was assigned to 1.1.8.
+ *
+ * @return bool
+ */
+function DOCUMENTS_updateConfig_1_1_2()
+{
+    return DOCUMENTS_updateConfig_1_1_8();
 }
