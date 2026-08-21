ea1140d8599c158d7e83fba7c9293263eb7a318a
Ben <hostellerie.org@gmail.com>
2026-08-21 15:24:11 +0200

    Documents 1.1.8: update French configuration labels

--- language/french_france_utf-8.php
+++ language/french_france_utf-8.php
@@ -2,15 +2,16 @@
 
 /* Reminder: always indent with 4 spaces (no tabs). */
 // +---------------------------------------------------------------------------+
-// | Documents Plugin 1.0                                                      |
+// | Documents Plugin 1.1.2                                                    |
 // +---------------------------------------------------------------------------+
-// | english.php                                                               |
+// | french_france_utf-8.php                                                   |
 // |                                                                           |
-// | English language file                                                     |
+// | French language file                                                      |
 // +---------------------------------------------------------------------------+
-// | Copyright (C) 2012 by the following authors:                              |
+// | Copyright (C) 2012-2026 by the following authors:                         |
 // |                                                                           |
 // | Authors: Ben - ben AT geeklog DOT fr                                      |
+// |          Documents plugin contributors                                    |
 // +---------------------------------------------------------------------------+
 // | Created with the Geeklog Plugin Toolkit.                                  |
 // +---------------------------------------------------------------------------+
@@ -35,105 +36,93 @@
 * @package Documents
 */
 
-/**
-* Import Geeklog plugin messages for reuse
-*
-* @global array $LANG32
-*/
 global $LANG32;
-
-// +---------------------------------------------------------------------------+
-// | Array Format:                                                             |
-// | $LANGXX[YY]:  $LANG - variable name                                       |
-// |               XX    - specific array name                                 |
-// |               YY    - phrase id or number                                 |
-// +---------------------------------------------------------------------------+
 
 $LANG_DOCUMENTS_1 = array(
     'plugin_name'     => 'Documents',
     'categories'      => 'Catégories',
 	'documents'       => 'Documents',
-	'category'        => 'Categorie',
+	'category'        => 'Catégorie',
 	'new_cat'         => 'Créer une nouvelle catégorie',
-	'edit_cat'        => 'Editer une catégorie',
+	'edit_cat'        => 'Éditer une catégorie',
 	'cat_name'        => 'Nom de la catégorie',
-	'cat_url'         => 'Nom pour l\'URL (sans d\'espace)', 
+	'cat_url'         => 'Nom pour l\'URL (sans espace)',
 	'cat_url_exists'  => 'Le nom pour l\'URL existe déjà. Merci de le changer.',
 	'css'             => 'CSS',
 	'template'        => 'Template',
-    'cat_order'	      => 'Ordre',
-	'existing_cat'    => 'Catégories existantes (ordre, nom et url)',
+    'cat_order'      => 'Ordre',
+	'existing_cat'    => 'Catégories existantes (ordre, nom et URL)',
 	'none'            => 'Aucune',
-	'cat_help'        => 'Help for submission form (can be a autotag)',
-	'list_index'      => 'Display this category in  the list',
-	'submitable'      => 'User can submit documents for this category',
-	'custom_header'   => 'Custom header',
-	'custom_footer'   => 'Custom footer',
+	'cat_help'        => 'Aide pour le formulaire de soumission (peut contenir un autotag)',
+	'list_index'      => 'Afficher cette catégorie dans la liste',
+	'submitable'      => 'Les utilisateurs peuvent proposer des documents dans cette catégorie',
+	'custom_header'   => 'En-tête personnalisé',
+	'custom_footer'   => 'Pied de page personnalisé',
 	'required_field'  => 'Indique un champ requis',
 	'admin'           => 'Admin',
 	'list_categories' => 'Catégories',
-	'edit'            => 'Editer',
+	'edit'            => 'Éditer',
 	'save_button'     => 'Sauvegarder',
-	'delete_button'   => 'Effacer',
+	'delete_button'   => 'Supprimer',
 	'error'           => 'Erreur',
 	'missing_field'   => 'Au moins un champ est manquant. Vérifiez :',
-	'save_fail'       => 'Sauvgarde échouée',
+	'save_fail'       => 'La sauvegarde a échoué',
 	'save_success'    => 'Sauvegarde réussie',
 	'delete_fail'     => 'La suppression a échoué',
 	'delete_success'  => 'La suppression a bien été effectuée',
 	'message'         => 'Message',
 	'validate_button' => 'OK',
 	'fields'          => 'Champs',
-	'new_field'       => 'Create a new field',
-	'list_fields'     => 'List of the fields',
-	'field_name'      => 'Name of the field',
-	'field_order'     => 'Field order',
-	'existing_field' => 'Champs existants (ordre et nom)',
-	'var_name'        => 'Name of the variable',
-	'field_help'      => 'Help for submission form',
+	'new_field'       => 'Créer un nouveau champ',
+	'list_fields'     => 'Liste des champs',
+	'field_name'      => 'Nom du champ',
+	'field_order'     => 'Ordre du champ',
+	'existing_field'  => 'Champs existants (ordre et nom)',
+	'var_name'        => 'Nom de la variable',
+	'field_help'      => 'Aide pour le formulaire de soumission',
 	'type'            => 'Type',
-	'sel_group'       => 'Select group',
+	'sel_group'       => 'Groupe de sélection',
 	'create_new_doc'  => 'Créer un nouveau document',
-	'field_require'   => 'Point out this is a required field on submission and edit forms',
-	'field_on_list'   => 'This field must be a column on the list of documents',
-	'edit_doc'        => 'Edition d\'un document',
-	'selects'         => 'Selects',
-	'list_groups'     => 'List of groups',
-	'new_group'       => 'Create a new group',
-	'list_selects'    => 'Liste des selects',
-	'new_select'      => 'Créer un nouveau select',
+	'field_require'   => 'Indiquer que ce champ est obligatoire dans les formulaires de soumission et d\'édition',
+	'field_on_list'   => 'Afficher ce champ comme colonne dans la liste des documents',
+	'edit_doc'        => 'Édition d\'un document',
+	'selects'         => 'Sélections',
+	'list_groups'     => 'Liste des groupes',
+	'new_group'       => 'Créer un nouveau groupe',
+	'list_selects'    => 'Liste des sélections',
+	'new_select'      => 'Créer une nouvelle sélection',
 	'new_option'      => 'Créer une nouvelle option',
-	'group'           => 'Group',
-	'group_name'      => 'Group name',
-	'group_help'      => 'Group help',
-	'edit_group'      => 'Edit group',
-	'select_name'     => 'Name (or option value)',
-	'select_value'    => 'Value to display for users',
-	'existing_select' => 'Existings selects',
-	'select_order'    => 'Select order',
-	'doc_submission'  => 'Document submission',
+	'group'           => 'Groupe',
+	'group_name'      => 'Nom du groupe',
+	'group_help'      => 'Aide du groupe',
+	'edit_group'      => 'Éditer le groupe',
+	'select_name'     => 'Nom (ou valeur de l\'option)',
+	'select_value'    => 'Valeur affichée aux utilisateurs',
+	'existing_select' => 'Sélections existantes',
+	'select_order'    => 'Ordre de la sélection',
+	'doc_submission'  => 'Soumission de document',
 	'submission'      => 'Soumission',
 	'not_active'      => 'Non actif',
 	'draft'           => 'Brouillon',
-	'active_label'    => 'En ligne',
-	'active'          => 'Active',
-	'submission_recorded' => 'Votre document a bien été sauvegardé. Il va être vérifié avant d\'être disponible en ligne. Merci.', 
+	'active_label'    => 'Document actif',
+	'active'          => 'Actif',
+	'submission_recorded' => 'Votre document a bien été sauvegardé. Il va être vérifié avant d\'être disponible en ligne. Merci.',
 	'submissions_list'    => 'Liste des soumissions',
 	'submissions_list_2'  => 'Liste des documents en cours d\'étude',
 	'drafts_list'         => 'Liste des documents en cours de rédaction',
 	'documents_list'      => 'Liste des documents',
 	'reserved_to'         => 'Pour accéder à ce document vous devez faire partie du groupe :',
 	'cat_hidden'          => 'Catégorie cachée',
-	'see_all_docs'        => 'Voir toutes les fiches',
+	'see_all_docs'        => 'Voir tous les documents',
 	'use_map'             => 'Utiliser la carte',
 	'use_map_details'     => 'Si vous avez besoin de géolocaliser chaque document, paramétrez ici la carte sur laquelle vous souhaitez ajouter les marqueurs.',
-	'nonactive'           => 'Inactive',
+	'nonactive'           => 'Inactif',
 	'limited_access'      => 'Accès limité',
 	'private'             => 'Privé',
 	'doc_by'              => 'Ce document proposé par',
-    'displayed'           => 'a été affiché',
+    'displayed'         => 'a été affiché',
 	'times'               => 'fois.',
-	'select_album'        => 'Selectionner l\'album :',
+	'select_album'        => 'Sélectionner l\'album :',
 	'no_map'              => '-- Aucune --',
 	'read_more_marker'    => 'Afficher le document',
 	'document_draft'      => 'Ce document est en mode brouillon. Son accès est réservé à son propriétaire.',
@@ -147,31 +136,35 @@ $PLG_documents_MESSAGE3002 = $LANG32[9]; // "requires a newer version of Geeklog
 // Localization of the Admin Configuration UI
 $LANG_configsections['documents'] = array(
     'label' => 'Documents',
-    'title' => 'Documents Configuration'
+    'title' => 'Configuration de Documents'
 );
 
 $LANG_confignames['documents'] = array(
-    'documents_folder'      => 'Document folder',
-    'documents_main_header' => 'Documents main header',
-    'documents_main_footer' => 'Documents main footer',
+    'documents_folder'      => 'Dossier public de Documents',
+    'documents_main_header' => 'En-tête principal de Documents',
+    'documents_main_footer' => 'Pied de page principal de Documents',
+    'max_image_width'       => 'Largeur maximale des images (pixels)',
+    'max_image_height'      => 'Hauteur maximale des images (pixels)',
+    'max_image_size'        => 'Taille maximale des fichiers image (octets)',
 );
 
 $LANG_configsubgroups['documents'] = array(
-    'sg_main' => 'Main Settings'
+    'sg_main' => 'Paramètres principaux'
 );
 
 $LANG_tab['documents'] = array(
-    'tab_main' => 'Documents Main Settings'
+    'tab_main' => 'Paramètres principaux de Documents'
 );
 
 $LANG_fs['documents'] = array(
-    'fs_main' => 'Documents Main Settings',
-	'fs_permissions'     => 'Default Permissions',
+    'fs_main'        => 'Paramètres principaux de Documents',
+    'fs_images'      => 'Images',
+	'fs_permissions' => 'Permissions par défaut',
 );
 
 $LANG_configselects['documents'] = array(
-    0 => array('True' => 1, 'False' => 0),
-    1 => array('True' => true, 'False' => false),
-	12 => array('No access' => 0, 'Read-Only' => 2, 'Read-Write' => 3),
+    0 => array('Oui' => 1, 'Non' => 0),
+    1 => array('Oui' => true, 'Non' => false),
+	12 => array('Aucun accès' => 0, 'Lecture seule' => 2, 'Lecture-écriture' => 3),
 );
 ?>
