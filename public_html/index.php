eee3c64b9f3a0936362f9665769baae990530a9a
Ben <hostellerie.org@gmail.com>
2026-08-21 08:31:36 +0200

    Documents 1.1.2: expose bootstrap diagnostics in browser

--- public_html/index.php
+++ public_html/index.php
@@ -35,9 +35,29 @@ error_reporting(E_ALL);
 @ini_set('display_startup_errors', '1');
 @ini_set('log_errors', '1');
 
-if (function_exists('COM_errorLog')) {
-    COM_errorLog('DOCUMENTS DEBUG D00 - public index entered; URI=' . (isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : 'unknown'));
+$documentsBootstrapDebug = isset($_GET['documents_debug']) && $_GET['documents_debug'] == '1';
+
+function DOCUMENTS_debugOutput($message)
+{
+    global $documentsBootstrapDebug;
+
+    if ($documentsBootstrapDebug) {
+        echo '<pre style="background:#fff;color:#000;padding:10px;border:1px solid #ccc">'
+            . htmlspecialchars($message, ENT_QUOTES, 'UTF-8')
+            . '</pre>';
+        @ob_flush();
+        @flush();
+    }
+
+    if (function_exists('COM_errorLog')) {
+        COM_errorLog($message);
+    }
 }
+
+DOCUMENTS_debugOutput(
+    'DOCUMENTS DEBUG B00 - public index entered; URI='
+    . (isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : 'unknown')
+);
 
 set_error_handler(function ($errno, $errstr, $errfile, $errline) {
     if (function_exists('COM_errorLog')) {
@@ -54,48 +74,40 @@ set_error_handler(function ($errno, $errstr, $errfile, $errline) {
 register_shutdown_function(function () {
     $error = error_get_last();
     if ($error !== null) {
+        $message = 'DOCUMENTS DEBUG SHUTDOWN - type=' . $error['type']
+            . ' message=' . $error['message']
+            . ' file=' . $error['file']
+            . ' line=' . $error['line'];
         if (function_exists('COM_errorLog')) {
-            COM_errorLog(
-                'DOCUMENTS DEBUG SHUTDOWN - type=' . $error['type']
-                . ' message=' . $error['message']
-                . ' file=' . $error['file']
-                . ' line=' . $error['line']
-            );
+            COM_errorLog($message);
         }
-    } else {
-        if (function_exists('COM_errorLog')) {
-            COM_errorLog('DOCUMENTS DEBUG SHUTDOWN - normal request end, no PHP fatal error');
+        if (isset($_GET['documents_debug']) && $_GET['documents_debug'] == '1') {
+            echo '<pre style="background:#fff;color:#900;padding:10px;border:1px solid #900">'
+                . htmlspecialchars($message, ENT_QUOTES, 'UTF-8')
+                . '</pre>';
         }
+    } elseif (function_exists('COM_errorLog')) {
+        COM_errorLog('DOCUMENTS DEBUG SHUTDOWN - normal request end, no PHP fatal error');
     }
 });
 
 if (!isset($_PLUGINS) || !is_array($_PLUGINS) || !in_array('documents', $_PLUGINS, true)) {
-    if (function_exists('COM_errorLog')) {
-        COM_errorLog('DOCUMENTS DEBUG D01 - plugin inactive, redirecting');
-    }
+    DOCUMENTS_debugOutput('DOCUMENTS DEBUG B01 - plugin inactive, redirecting');
     echo COM_refresh($_CONF['site_url'] . '/index.php');
     exit;
 }
 
-if (function_exists('COM_errorLog')) {
-    COM_errorLog('DOCUMENTS DEBUG D02 - plugin active; loading include_compat.php');
-}
+DOCUMENTS_debugOutput('DOCUMENTS DEBUG B02 - plugin active; loading include_compat.php');
 require_once $_CONF['path'] . 'plugins/documents/include_compat.php';
 
-if (function_exists('COM_errorLog')) {
-    COM_errorLog('DOCUMENTS DEBUG D03 - include_compat.php loaded; initializing request defaults');
-}
+DOCUMENTS_debugOutput('DOCUMENTS DEBUG B03 - include_compat.php loaded; initializing request defaults');
 DOCUMENTS_initializeRequestDefaults($_REQUEST);
 
-if (function_exists('COM_errorLog')) {
-    COM_errorLog(
-        'DOCUMENTS DEBUG D04 - before include_html.php; mode='
-        . (isset($_REQUEST['mode']) ? $_REQUEST['mode'] : '')
-        . ' cat=' . (isset($_REQUEST['cat']) ? $_REQUEST['cat'] : '')
-    );
-}
+DOCUMENTS_debugOutput(
+    'DOCUMENTS DEBUG B04 - before include_html.php; mode='
+    . (isset($_REQUEST['mode']) ? $_REQUEST['mode'] : '')
+    . ' cat=' . (isset($_REQUEST['cat']) ? $_REQUEST['cat'] : '')
+);
 require_once $_CONF['path'] . 'plugins/documents/include_html.php';
 
-if (function_exists('COM_errorLog')) {
-    COM_errorLog('DOCUMENTS DEBUG D99 - include_html.php returned normally');
-}
+DOCUMENTS_debugOutput('DOCUMENTS DEBUG B99 - include_html.php returned normally');
