017fe641e749eb417c2cdb03039103bcbc6112e2
Ben <hostellerie.org@gmail.com>
2026-08-22 15:58:35 +0200

    Documents 1.1.10: test public statistics visibility

--- tests/presentation_test.php
+++ tests/presentation_test.php
@@ -28,4 +28,34 @@ $html = DOCUMENTS_textFormatOptions(1002, array(
 presentation_assert(strpos($html, 'value="1002" selected="selected"') !== false, 'selected format is not preserved');
 presentation_assert(strpos($html, 'value="-') === false, 'text format options must survive numeric request filtering');
 
+$GLOBALS['documents_test_admin'] = false;
+$GLOBALS['documents_test_anon'] = true;
+function SEC_hasRights($rights)
+{
+    return !empty($GLOBALS['documents_test_admin']);
+}
+function COM_isAnonUser()
+{
+    return !empty($GLOBALS['documents_test_anon']);
+}
+
+$_DOCUMENTS_CONF = array('stats_visibility' => 0);
+presentation_assert(DOCUMENTS_canShowStats() === false, 'hidden stats should be hidden');
+
+$_DOCUMENTS_CONF['stats_visibility'] = 1;
+presentation_assert(DOCUMENTS_canShowStats() === false, 'anonymous visitor saw admin-only stats');
+$GLOBALS['documents_test_admin'] = true;
+presentation_assert(DOCUMENTS_canShowStats() === true, 'administrator could not see admin-only stats');
+
+$GLOBALS['documents_test_admin'] = false;
+$_DOCUMENTS_CONF['stats_visibility'] = 2;
+$GLOBALS['documents_test_anon'] = true;
+presentation_assert(DOCUMENTS_canShowStats() === false, 'anonymous visitor saw member stats');
+$GLOBALS['documents_test_anon'] = false;
+presentation_assert(DOCUMENTS_canShowStats() === true, 'logged-in user could not see member stats');
+
+$_DOCUMENTS_CONF['stats_visibility'] = 3;
+$GLOBALS['documents_test_anon'] = true;
+presentation_assert(DOCUMENTS_canShowStats() === true, 'anonymous visitor could not see public stats');
+
 echo "Documents presentation tests: PASS\n";
