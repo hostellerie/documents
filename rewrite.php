dcb38dd69ca2bb16e23d45883dcd543605131013
Ben <hostellerie.org@gmail.com>
2026-08-29 22:06:06 +0200

    Route standard document saves through secure dispatcher

--- rewrite.php
+++ rewrite.php
@@ -53,6 +53,8 @@ if (!function_exists('DOCUMENTS_writeHtaccess')) {
         $rules = "RewriteEngine On\n\n"
             . "RewriteCond %{QUERY_STRING} (^|&)mode=edit_cat(&|$)\n"
             . "RewriteRule ^index\\.php$ category-editor.php [L,QSA]\n\n"
+            . "RewriteCond %{QUERY_STRING} (^|&)mode=save(&|$)\n"
+            . "RewriteRule ^index\\.php$ document-save.php [L,QSA]\n\n"
             . "RewriteRule ^$ home.php [L]\n\n"
             . "RewriteCond %{REQUEST_FILENAME} !-f\n"
             . "RewriteCond %{REQUEST_FILENAME} !-d\n"
