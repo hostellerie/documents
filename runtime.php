faf6c4e1f8cbcf802d8603b3eb520fe0d1a048dd
Ben <hostellerie.org@gmail.com>
2026-09-01 15:58:46 +0200

    Load Documents integrity helpers in runtime

--- runtime.php
+++ runtime.php
@@ -14,6 +14,7 @@
 if (isset($_CONF['path'])) {
     $documentsRuntimeFiles = array(
         'security.php',
+        'integrity.php',
         'navigation.php',
         'page_layout.php',
         'mediagallery_adapter.php',
